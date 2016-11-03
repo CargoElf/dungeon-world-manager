23b57f29a04e962eaf5747ad7026c60bba32b8aa
Ben Van Sickle <email": null>
2016-11-03 18:03:41 -0500

    add basic virtual attributes for ability mods

--- app/models/player_character.rb
+++ app/models/player_character.rb
@@ -1,3 +1,33 @@
 class PlayerCharacter < ApplicationRecord
   validates_presence_of :name, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma
+
+  def str_mod
+    mod(self.strength)
+  end
+
+  def dex_mod
+    mod(self.dexterity)
+  end
+
+  def con_mod
+    mod(self.constitution)
+  end
+
+  def int_mod
+    mod(self.intelligence)
+  end
+
+  def wis_mod
+    mod(self.wisdom)
+  end
+
+  def cha_mod
+    mod(self.charisma)
+  end
+
+  private
+
+  def mod(attribute)
+    attribute
+  end
 end
