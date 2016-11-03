501788cf11301edd91ce9fc989d55ca107338130
Ben Van Sickle <email": null>
2016-11-03 17:14:32 -0500

    create index view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :player_character
 
-  root "player_characters#show"
+  root "player_characters#index"
 
 end
