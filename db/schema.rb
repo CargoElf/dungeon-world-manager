4d4ff54dca7f38ad756c6e065af608c203101356
Ben Van Sickle <email": null>
2016-11-11 09:28:13 -0600

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,91 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103205842) do
+ActiveRecord::Schema.define(version: 20161111152603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "alignments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "bonds", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "character_classes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "class_alignments", force: :cascade do |t|
+    t.integer  "character_class_id"
+    t.integer  "alignment_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["alignment_id"], name: "index_class_alignments_on_alignment_id", using: :btree
+    t.index ["character_class_id"], name: "index_class_alignments_on_character_class_id", using: :btree
+  end
+
+  create_table "class_bonds", force: :cascade do |t|
+    t.integer  "character_class_id"
+    t.integer  "bond_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["bond_id"], name: "index_class_bonds_on_bond_id", using: :btree
+    t.index ["character_class_id"], name: "index_class_bonds_on_character_class_id", using: :btree
+  end
+
+  create_table "class_moves", force: :cascade do |t|
+    t.integer  "move_level"
+    t.integer  "character_class_id"
+    t.integer  "move_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["character_class_id"], name: "index_class_moves_on_character_class_id", using: :btree
+    t.index ["move_id"], name: "index_class_moves_on_move_id", using: :btree
+  end
+
+  create_table "class_races", force: :cascade do |t|
+    t.integer  "character_class_id"
+    t.integer  "race_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["character_class_id"], name: "index_class_races_on_character_class_id", using: :btree
+    t.index ["race_id"], name: "index_class_races_on_race_id", using: :btree
+  end
+
+  create_table "gears", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "moves", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "player_characters", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.integer  "level",          default: 1
-    t.integer  "strength"
-    t.integer  "dexterity"
-    t.integer  "constitution"
-    t.integer  "intelligence"
-    t.integer  "wisdom"
-    t.integer  "charisma"
+    t.integer  "strength",       default: 1
+    t.integer  "dexterity",      default: 1
+    t.integer  "constitution",   default: 1
+    t.integer  "intelligence",   default: 1
+    t.integer  "wisdom",         default: 1
+    t.integer  "charisma",       default: 1
     t.text     "inventory"
     t.integer  "player_id"
     t.integer  "game_master_id"
@@ -32,4 +102,30 @@ ActiveRecord::Schema.define(version: 20161103205842) do
     t.datetime "updated_at",                 null: false
   end
 
+  create_table "races", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "starting_gears", force: :cascade do |t|
+    t.integer  "character_class_id"
+    t.integer  "gear_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["character_class_id"], name: "index_starting_gears_on_character_class_id", using: :btree
+    t.index ["gear_id"], name: "index_starting_gears_on_gear_id", using: :btree
+  end
+
+  add_foreign_key "class_alignments", "alignments"
+  add_foreign_key "class_alignments", "character_classes"
+  add_foreign_key "class_bonds", "bonds"
+  add_foreign_key "class_bonds", "character_classes"
+  add_foreign_key "class_moves", "character_classes"
+  add_foreign_key "class_moves", "moves"
+  add_foreign_key "class_races", "character_classes"
+  add_foreign_key "class_races", "races"
+  add_foreign_key "starting_gears", "character_classes"
+  add_foreign_key "starting_gears", "gears"
 end
