d5d8ad739072c232e3262586bae6942265eef333
Ben Van Sickle <email": null>
2016-11-11 09:18:21 -0600

    generate character class and outer tables

---
+++ db/migrate/20161111151615_create_moves.rb
@@ -0,0 +1,10 @@
+class CreateMoves < ActiveRecord::Migration[5.0]
+  def change
+    create_table :moves do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20161111151638_create_alignments.rb
@@ -0,0 +1,10 @@
+class CreateAlignments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :alignments do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
