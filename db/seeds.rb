5e40ce861b8541b4209cb714a35527cdbdeb012b
Ben Van Sickle <email": null>
2016-12-11 16:33:45 -0600

    begin setup of seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "theif_seed"
+
+thief = PlayerCharacter.create!(thief_pc_seed)
