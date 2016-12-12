a1dfccb98e103531fe538c61157a4cd0e8be9c6f
Ben Van Sickle <email": null>
2016-12-11 18:48:00 -0600

    Nested attribues submitting

--- app/controllers/player_characters_controller.rb
+++ app/controllers/player_characters_controller.rb
@@ -5,11 +5,13 @@ class PlayerCharactersController < ApplicationController
   end
 
   def show
-    @player_character = PlayerCharacter.includes(:alignment, :race).where(id: params[:id]).first
+    @player_character = PlayerCharacter.includes(:alignment, :race, :moves, :bonds).where(id: params[:id]).first
   end
 
   def new
     @player_character = PlayerCharacter.new
+    @race = @player_character.build_race
+    @alignment = @player_character.build_alignment
   end
 
   def create
@@ -23,7 +25,7 @@ class PlayerCharactersController < ApplicationController
   end
 
   def edit
-    @player_character = PlayerCharacter.find(:id)
+    @player_character = PlayerCharacter.includes(:alignment, :race, :moves, :bonds).where(id: params[:id]).first
   end
 
   def update
@@ -32,11 +34,7 @@ class PlayerCharactersController < ApplicationController
   private
 
   def player_character_params
-    params.require(:player_character).permit(:name, :level, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma)
-  end
-
-  def class_params
-    params.require
+    params.require(:player_character).permit(:name, :class_name, :level, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma, race_attributes: [:name, :description], alignment_attributes: [:name, :description])
   end
 
 end
