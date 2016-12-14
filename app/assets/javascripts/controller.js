f7ee36f35a8ca9419b5671edbbb8f483f1263e78
Ben Van Sickle <email": null>
2016-12-14 09:24:28 -0600

    Add js to not allow adding new moves unless the last box has a name and description

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -4,21 +4,3 @@ $(document).ready(function() {
     addMove()
     addBond()
 });
-
-function addMove() {
-    var num = $(".moves-input").children().size() + 1;
-    $(".moves-button").on("click", function () {
-      $(".moves-input").append("<input type=\"text\" name=\"moves[" + num + "][name]\" placeholder=\"Description Name\" class=\"moves-name\"><textarea name=\"moves[" + num + "][description]\" placeholder=\"Description of Move\" class=\"moves-description form-control\"></textarea>");
-      num++
-    })
-}
-
-function addBond() {
-    var num = $(".bonds-input").children().size() + 1;
-    $(".bonds-button").on("click", function () {
-      if ($(".bond-description").last().val() != "") {
-        $(".bonds-input").append("<textarea name=\"bonds[" + num + "][description]\" placeholder=\"Description of Bond\" class=\"bond-description form-control\"></textarea>");
-      }
-      num++
-    })
-}
---
+++ app/assets/javascripts/view.js
@@ -0,0 +1,19 @@
+function addMove() {
+    var num = $(".moves-input").children().size() + 1;
+    $(".moves-button").on("click", function () {
+    	if ($(".moves-description").last().val() != "" && $(".moves-name").last().val() != ""){
+				$(".moves-input").append("<input type=\"text\" name=\"moves[" + num + "][name]\" placeholder=\"Description Name\" class=\"moves-name\"><textarea name=\"moves[" + num + "][description]\" placeholder=\"Description of Move\" class=\"moves-description form-control\"></textarea>");
+				num++
+			}
+    })
+}
+
+function addBond() {
+    var num = $(".bonds-input").children().size() + 1;
+    $(".bonds-button").on("click", function () {
+      if ($(".bond-description").last().val() != "") {
+        $(".bonds-input").append("<textarea name=\"bonds[" + num + "][description]\" placeholder=\"Description of Bond\" class=\"bond-description form-control\"></textarea>");
+				num++
+      }
+    })
+}
